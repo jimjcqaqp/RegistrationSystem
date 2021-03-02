29dc5ba61959c7c240349a5b4b955907aedcc3a2
Jimmy Chullunquia Quispe <jimmy.jcq.net@gmail.com>
2021-03-02 10:11:21 -0500

    start menu teacher

--- menu_teacher.cc
+++ menu_teacher.cc
@@ -1,6 +1,8 @@
 #include "menu_teacher.h"
+#include "Grade.h"
 
 void menu_teacher_postlogin(Teacher t);
+void workload();
 
 void menu_teacher()
 {
@@ -38,8 +40,35 @@ void menu_teacher_postlogin(Teacher t){
 
 	while(true){
 		int indice = m.start();
+		switch(indice){
+			case 0: 
+				workload();
+				break;
+			case 1:
+				break;
+		}
 		if(indice == 2)
 			break;
 	}
 }
+
+void workload(){
+/*	Menu m;
+	std::vector<Grade> gs = Grade::allByCourseId();
+	for(int i = 0; i < ts.size(); ++i)
+		m.add("");
+
+	m.width = 50;
+	m.center(false, true);
+	m.height = 10;
+	m.y = 20;
+	m.title = "Todos los docentes";
+	m.footer = std::to_string(ts.size()) + " profesores";
+	m.mark = "";
+	m.hide = true;
+
+	m.start();
+*/
+}
+
 
